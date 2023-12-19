b37562365b07a73c10c9a1e7b7588b6893f9f22a
codnjs183 <chaewon1830@gmail.com>
2023-12-19 18:43:59 +0900

    23/12/19 수업 내용

--- src/main/java/com/quiz/lesson03/Lesson03Quiz01RestController.java
+++ src/main/java/com/quiz/lesson03/Lesson03Quiz01RestController.java
@@ -34,6 +34,10 @@ public class Lesson03Quiz01RestController {
 	// url: http://localhost/lesson03/quiz01/3?area=90&price=130000
 	@RequestMapping("/3")
 	public List<RealEstate> quiz01_3(
+			// myBatis 문법 상 파라미터는 단 한개만 xml로 보낼 수 있다.
+			// 파라미터들을 하나의 맵에 담아서 보낸다.
+			// 맵으로 만들어주는 어노테이션: @Param
+			// @Param으로 데이터를 보낼 경우, xml에서 받는 타입의 경우 int가 아닌 map이어야 한다.
 			@RequestParam(value = "area", defaultValue = "0") int area, 
 			@RequestParam(value = "price", defaultValue = "0") int price) {
 		return realEstateBO.getRealEstateListByAreaPrice(area, price);
--- src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
+++ src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
@@ -2,6 +2,7 @@ package com.quiz.lesson03.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quiz.lesson03.domain.RealEstate;
@@ -14,4 +15,15 @@ public interface RealEstateMapper {
 	public List<RealEstate> selectRealEstateListByRentPrice(int rentPrice);
 	
 	public List<RealEstate> selectRealEstateListByAreaPrice(int area, int price);
+	
+	public int insertRealEstate(RealEstate realEstate);
+	
+	public int insertRealEstateAsField(
+			@Param("realtorId") int realtorId, 
+			@Param("address") String address, 
+			@Param("area") int area, 
+			@Param("type") String type, 
+			@Param("price") int price, 
+			@Param("rentPrice") Integer rentPrice
+			);
 }
