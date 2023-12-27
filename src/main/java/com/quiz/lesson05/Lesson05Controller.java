81bb29833e4329dfeab4374a8f6e35262dc316b2
codnjs183 <chaewon1830@gmail.com>
2023-12-27 18:43:37 +0900

    23/12/27 수업 내용

--- src/main/java/com/quiz/lesson05/Lesson05Controller.java
+++ src/main/java/com/quiz/lesson05/Lesson05Controller.java
@@ -1,6 +1,10 @@
 package com.quiz.lesson05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +15,17 @@ public class Lesson05Controller {
 	@GetMapping("/quiz01")
 	public String quiz01() {
 		return "lesson05/quiz01";
+	}
+
+	@GetMapping("/quiz03")
+	public String quiz03(Model model) {
+		List<Integer> candidates = new ArrayList<>();
+		candidates.add(263001);
+		candidates.add(173942); 
+		candidates.add(563057); 
+		
+		model.addAttribute("candidates", candidates);
+		
+		return "lesson05/quiz03";
 	}
 }
